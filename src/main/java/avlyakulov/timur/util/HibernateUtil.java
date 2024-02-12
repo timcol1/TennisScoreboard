606b2af760939451b00ce357ef6abc16fc78f303
Timur <91984444+timcol1@users.noreply.github.com>
2024-02-12 14:22:44 +0200

    add changes

--- src/main/java/avlyakulov/timur/util/HibernateUtil.java
+++ src/main/java/avlyakulov/timur/util/HibernateUtil.java
@@ -1,5 +1,7 @@
 package avlyakulov.timur.util;
 
+import avlyakulov.timur.other.model.Person;
+import avlyakulov.timur.other.model.PersonTask;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -8,12 +10,15 @@ import java.util.Properties;
 
 public class HibernateUtil {
 
-    public static SessionFactory getSessionFactory(Class<?> clazz) throws IOException {
+    public static SessionFactory getSessionFactory() throws IOException {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-
         Properties hibernateProperty = new Properties();
         hibernateProperty.load(classLoader.getResourceAsStream("postgres.properties"));
 
-        return new Configuration().addAnnotatedClass(clazz).addProperties(hibernateProperty).buildSessionFactory();
+        return new Configuration()
+                .addAnnotatedClass(Person.class)
+                .addAnnotatedClass(PersonTask.class)
+                .addProperties(hibernateProperty)
+                .buildSessionFactory();
     }
 }
